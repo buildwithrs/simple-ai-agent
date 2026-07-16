b2cc9255a7def911e6c2cf68cb10491b29108793
forfd8960 <forfd8960@gmail.com>
2026-07-16 10:45:21 +0800

    feat: adding cmd line input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use rustyline::DefaultEditor;
+
+const CMD_HIS: &'static str = ".history/history.txt";
+
+fn main() -> anyhow::Result<()> {
+    let mut rl = DefaultEditor::new()?;
+    rl.load_history(CMD_HIS).ok();
+
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str())?;
+                println!("You entered: {}", line);
+            }
+            Err(rustyline::error::ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(rustyline::error::ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+
+    rl.save_history(CMD_HIS).ok();
+    Ok(())
 }
